ea201035c80a01b8a8920694e0de775bbec8d122
Manyakbot <skylinedelfly@gmail.com>
2024-07-21 09:06:39 +0500

    '1'

---
+++ 12-create-object/finish.js
@@ -0,0 +1,16 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+const obj = {
+    name: 'Oleg',
+    surname: 'Mayokovski',
+    favoriteNumber: 13,
+}
+console.log(`My name is ${obj.name} ${obj.surname} and my favorite number is ${obj.favoriteNumber}`)
